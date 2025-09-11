2e44eabb61298f713dbc8161880637bffe79d375
fuka <fuka.bigkatsu0531@icloud.com>
2025-09-11 13:10:10 +0900

    a

--- Project/GameProject/Game/Goal.cpp
+++ Project/GameProject/Game/Goal.cpp
@@ -5,22 +5,20 @@ Goal::Goal(const CVector2D& pos):Base(eType_Goal)
 	m_pos = pos;
 	m_goal = COPY_RESOURCE("Goal", CImage);
 	m_goal.SetCenter(330/2,291/2);
-	m_rect = CRect(300,-150,630,150);
+	m_rect = CRect(-160,-150,200,200);
 }
 
 void Goal::Update()
 {
-	const float move_speed = 1.9;
-	m_pos.x += -move_speed;
 }
 
 void Goal::Draw()
 {
 	m_goal.SetSize(110*3.5,97*3.5);
-	m_goal.SetPos(m_pos);
+	m_goal.SetPos(GetScreenPos(m_pos));
 	m_goal.Draw();
 
-	//DrawRect();
+	DrawRect();
 }
 
 void Goal::Collision(Base* b)
